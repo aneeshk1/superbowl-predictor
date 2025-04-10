eb267ce2f0b5060657965c95f5b66be952e8bce4
Aneesh Kapole <aneeshkapole.9@gmail.com>
2025-04-09 22:07:11 -0400

    Hide Mongo Atlas credentials

--- stats_add.py
+++ stats_add.py
@@ -1,7 +1,7 @@
 from pymongo import MongoClient
 import pandas as pd
 
-MONGO_URI = "mongodb+srv://aneesh:stanley5@cluster65664.az03p.mongodb.net/"
+MONGO_URI = "mongodb+srv://aneesh:<db_password>@cluster65664.az03p.mongodb.net/"
 DB_NAME = "Project-CSE-482"
 
 def flatten_header(l1: str, l2: str):
